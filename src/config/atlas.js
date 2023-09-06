6a3c9cb710d82d10f189ed9680d5ec64eb17f75f
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-06 16:55:47 -0500

    feat: :sparkles: Conexion y creacion de base de datos

--- src/config/atlas.js
+++ src/config/atlas.js
@@ -4,12 +4,8 @@ dotenv.config("../");
 
 async function conexion() {
   try {
-    const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@edwarcorredor.zavkvu2.mongodb.net/${process.env.ATLAS_DB}`;
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-    const client = await MongoClient.connect(uri, options);
+    const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.zarbjab.mongodb.net/${process.env.ATLAS_DB}`;
+    const client = await MongoClient.connect(uri);
     return client.db();
   } catch (error) {
     return {status: 500, message: error};
